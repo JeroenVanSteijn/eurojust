753f7b6d1841118e0625804b1a701469b2b64763
Jeroen van Steijn <jeroen@bytecode.nl>
2020-10-16 09:56:38 +0200

    Change backgorund

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ function App() {
     <div className="App">
       <section>
         <a href={nowaste} download>
+          <h1>
+            Are you ready to enter the Coordination Center and lead an Action Day?
+          </h1>
           <img alt="The Eurojust game" src={game}></img>
           <div>
-            <h2>
-              Are you ready to enter the Coordination Center and lead an Action Day?
-            </h2>
-          </div>
           <p>Click here on on the image to download the .ppsx file to start the game.</p>
           <p>The game requires microsoft office to be installed.</p>
           <p>Good luck cracking the case!</p>
           <p><i>Tip: for optimal user experience, it is advised to play the game (downloading is required) on your computer/laptop. The file may be too large for smartphones.</i></p>
+          </div>
         </a>
       </section>
     </div>
