de19740f7f9f1a54b9d3bcdd73cfb9af643ad0d5
Jeroen van Steijn <jeroen@bytecode.nl>
2021-03-10 10:06:48 +0100

    Fix text

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
           </h1>
           <img alt="The Eurojust game" src={game}></img>
           <div>
-          <p>Click here on on the image to download the .ppsx file to start the game.</p>
-          <p>The game requires microsoft office to be installed.</p>
+          <p>Click here or on the image to download the .ppsx file to start the game.</p>
+          <p>The game requires Microsoft Office to be installed.</p>
           <p>Good luck cracking the case!</p>
           <p><i>Tip: for optimal user experience, it is advised to play the game (downloading is required) on your computer/laptop. The file may be too large for smartphones.</i></p>
           </div>
